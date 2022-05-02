4ea530e1533bc848d76cccde8081e85748de9464
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-02 16:31:55 -0300

    Handling files with File and FileInfo classes

---
+++ Files/Program.cs
@@ -0,0 +1,25 @@
+﻿string sourcePath = @"D:\Projects\Udemy\Programação orientada a objetos - C#\CSharpCourse\CSharpCourse\Files\Temp\file1.txt";
+string targetPath = @"D:\Projects\Udemy\Programação orientada a objetos - C#\CSharpCourse\CSharpCourse\Files\Temp\file2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    fileInfo.CopyTo(targetPath);
+    Console.WriteLine($"File copied successfully to {targetPath}");
+    Console.WriteLine();
+
+    string[] lines = File.ReadAllLines(sourcePath);
+    Console.WriteLine("The content of the file copied is:");
+
+    foreach (string line in lines)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(line);
+    }
+
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
+catch (IOException ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
