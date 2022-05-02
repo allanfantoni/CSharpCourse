0babb2d750f23ef6ad4e4be69cbb880d36f3b863
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-02 16:47:13 -0300

    FileStream and StreamReader classes

--- Files/Program.cs
+++ Files/Program.cs
@@ -1,25 +1,82 @@
 ﻿string sourcePath = @"D:\Projects\Udemy\Programação orientada a objetos - C#\CSharpCourse\CSharpCourse\Files\Temp\file1.txt";
 string targetPath = @"D:\Projects\Udemy\Programação orientada a objetos - C#\CSharpCourse\CSharpCourse\Files\Temp\file2.txt";
 
+#region First steps handling files with File and FileInfo classes
+
+//try
+//{
+//    FileInfo fileInfo = new FileInfo(sourcePath);
+//    fileInfo.CopyTo(targetPath);
+//    Console.WriteLine($"File copied successfully to {targetPath}");
+//    Console.WriteLine();
+
+//    string[] lines = File.ReadAllLines(sourcePath);
+//    Console.WriteLine("The content of the file copied is:");
+
+//    foreach (string line in lines)
+//    {
+//        Console.ForegroundColor = ConsoleColor.Yellow;
+//        Console.WriteLine(line);
+//    }
+
+//    Console.ForegroundColor = ConsoleColor.Gray;
+//}
+//catch (IOException ex)
+//{
+//    Console.WriteLine("An error occurred: " + ex.Message);
+//}
+
+#endregion
+
+#region FileStream and StreamReader
+
+//FileStream fs = null;
+//StreamReader sr = null;
+
+//try
+//{
+//    fs = new FileStream(sourcePath, FileMode.Open);
+//    sr = new StreamReader(fs);
+//    string line = sr.ReadLine();
+//    Console.WriteLine(line);
+//}
+//catch (IOException ex)
+//{
+//    Console.WriteLine($"An error occurred: {ex.Message}");
+//}
+//finally
+//{
+//    if (fs != null)
+//        fs.Close();
+    
+//    if (sr != null)
+//        sr.Close();
+//}
+
+#endregion
+
+#region Another example of using StreamReader without FileStream
+
+StreamReader sr = null;
+
 try
 {
-    FileInfo fileInfo = new FileInfo(sourcePath);
-    fileInfo.CopyTo(targetPath);
-    Console.WriteLine($"File copied successfully to {targetPath}");
-    Console.WriteLine();
+    sr = File.OpenText(sourcePath);
 
-    string[] lines = File.ReadAllLines(sourcePath);
-    Console.WriteLine("The content of the file copied is:");
-
-    foreach (string line in lines)
+    while (!sr.EndOfStream)
     {
-        Console.ForegroundColor = ConsoleColor.Yellow;
+        string line = sr.ReadLine();
         Console.WriteLine(line);
     }
-
-    Console.ForegroundColor = ConsoleColor.Gray;
 }
 catch (IOException ex)
 {
-    Console.WriteLine("An error occurred: " + ex.Message);
+    Console.WriteLine($"An error occurred: {ex.Message}");
 }
+finally
+{
+    if (sr != null)
+        sr.Close();
+}
+
+#endregion
