68fe5740ecaad6eb7adedc0d49dca416b983e770
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-02 19:32:57 -0300

    Created ProductsSold project, handling files

---
+++ ProductsSold/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductsSold.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total() => Price * Quantity;
+    }
+}
---
+++ ProductsSold/Program.cs
@@ -0,0 +1,40 @@
+﻿using ProductsSold.Entities;
+
+try
+{
+    Console.Write("Enter full path file: ");
+    string sourceFilePath = Console.ReadLine();
+
+    string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+    string targetFolderPath = sourceFolderPath + @"\Out";
+    string targetFilePath = targetFolderPath + @"\Summary.csv";
+
+    string[] lines = File.ReadAllLines(sourceFilePath);
+    
+    string[] fields = new string[3];
+    
+    string name = string.Empty;
+    double price = 0;
+    int quantity = 0;
+
+    Directory.CreateDirectory(targetFolderPath);
+
+    using (StreamWriter sw = File.AppendText(targetFilePath))
+    {
+        foreach (string line in lines)
+        {
+            fields = line.Split(',');
+            name = fields[0];
+            price = double.Parse(fields[1]);
+            quantity = int.Parse(fields[2]);
+
+            Product product = new Product(name, price, quantity);
+
+            sw.WriteLine($"{product.Name},{product.Total().ToString("F2")}");
+        }
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
