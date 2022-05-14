20b910b0ab2eea9faacffa114cf2ddb198aa1cc7
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-14 19:14:35 -0300

    First implementations of a project with Generics

---
+++ PrintValues/Program.cs
@@ -0,0 +1,16 @@
+﻿using PrintValues;
+
+PrintService printService = new();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+
+printService.Print();
+
+Console.WriteLine($"First: {printService.First()}");
