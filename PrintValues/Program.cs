71e4e5bafd1f68d00fee6f1fc6e93bbbc61c99aa
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-14 20:16:19 -0300

    Final implementations of a project with Generics

--- PrintValues/Program.cs
+++ PrintValues/Program.cs
@@ -1,16 +1,27 @@
 ﻿using PrintValues;
 
-PrintService printService = new();
-
-Console.Write("How many values? ");
-int n = int.Parse(Console.ReadLine());
-
-for (int i = 0; i < n; i++)
+try
 {
-    int x = int.Parse(Console.ReadLine());
-    printService.AddValue(x);
+    PrintService<int> printService = new();
+
+    Console.Write("How many values? ");
+    int n = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < n; i++)
+    {
+        int x = int.Parse(Console.ReadLine());
+        printService.AddValue(x);
+    }
+
+    printService.Print();
+
+    Console.WriteLine($"First: {printService.First()}");
 }
-
-printService.Print();
-
-Console.WriteLine($"First: {printService.First()}");
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
