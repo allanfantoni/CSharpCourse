ac396bf916839697d8aaffe81c33bdcfd0abb49d
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-15 22:09:00 -0300

    Constraints on type parameters

---
+++ CalculateGreaterValue/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace CalculateGreaterValue.Entities
+{
+    public class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string? ToString() => $"{Name}, {Price:F2}";
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is not Product)
+                throw new ArgumentException("Comparing error: argument is not a product.");
+
+            Product? other = obj as Product;
+
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ CalculateGreaterValue/Program.cs
@@ -0,0 +1,33 @@
+﻿using CalculateGreaterValue.Entities;
+using CalculateGreaterValue.Services;
+
+try
+{
+    List<Product> list = new();
+
+    Console.Write("Enter N: ");
+    int n = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < n; i++)
+    {
+        string[] vect = Console.ReadLine().Split(',');
+        string name = vect[0];
+        double price = double.Parse(vect[1]);
+        list.Add(new Product(name, price));
+    }
+
+    CalculationService calculationService = new();
+
+    Product max = calculationService.Max(list);
+
+    Console.WriteLine("Max:");
+    Console.WriteLine(max);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
