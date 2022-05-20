f3b74a645480e61638a9fd5c4f205d0d3006c82c
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-19 23:46:05 -0300

    Equals and GetHashCode examples

---
+++ ComparingClients/Program.cs
@@ -0,0 +1,18 @@
+﻿using ComparingClients.Entities;
+
+Client a = new() 
+{
+    Name = "Maria",
+    Email = "maria@gmail.com"
+};
+
+Client b = new()
+{
+    Name = "Alex",
+    Email = "alex@gmail.com"
+};
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(a == b);
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
