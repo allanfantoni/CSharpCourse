ca6738d025e49dcd82f0b0e33697967dd8fd9d44
allanfantoni <allan.fantoni@outlook.com>
2022-05-20 11:07:41 -0300

    Comparing collections implementing GetHashCode and Equals methods

---
+++ ComparingCollections/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace ComparingCollections.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override int GetHashCode() => Name.GetHashCode() + Price.GetHashCode();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Product)
+                return false;
+
+            Product? other = obj as Product;
+
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
---
+++ ComparingCollections/Program.cs
@@ -0,0 +1,18 @@
+﻿using ComparingCollections.Entities;
+
+HashSet<Product> a = new();
+
+a.Add(new Product("TV", 900.0));
+a.Add(new Product("Notebook", 1200.0));
+
+HashSet<Point> b = new();
+
+b.Add(new Point(3, 4));
+b.Add(new Point(5, 10));
+
+Product prod = new("Notebook", 1200.0);
+
+Console.WriteLine(a.Contains(prod));
+
+Point p = new(5, 10);
+Console.WriteLine(b.Contains(p));
