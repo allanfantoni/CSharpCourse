1d5a4de632cadee3acdbea1da35c52830f7ca57e
allanfantoni <allan.fantoni@outlook.com>
2022-05-20 12:00:52 -0300

    Log record using StreamReader to read a file and counting how many users accessed using GetHashCode and Equals.

---
+++ AccessLog/Entities/LogRecord.cs
@@ -0,0 +1,20 @@
+﻿namespace AccessLog.Entities
+{
+    public class LogRecord
+    {
+        public string? Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode() => Username.GetHashCode();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not LogRecord)
+                return false;
+
+            LogRecord? other = obj as LogRecord;
+
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ AccessLog/Program.cs
@@ -0,0 +1,30 @@
+﻿using AccessLog.Entities;
+
+HashSet<LogRecord> set = new();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(' ');
+            string name = line[0];
+            DateTime instant = DateTime.Parse(line[1]);
+            set.Add(new LogRecord 
+            { 
+                Username = name, 
+                Instant = instant 
+            });
+        }
+
+        Console.WriteLine("Total users: " + set.Count);
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
