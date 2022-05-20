9f1d283de5ae99ff117a36da1fed4fe8052c7ffe
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-20 00:47:11 -0300

    Using SortedSet objects

--- ComparingClients/Entities/Client.cs
+++ ComparingClients/Entities/Client.cs
@@ -2,22 +2,19 @@
 {
     public class Client
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
 
         public override bool Equals(object? obj)
         {
-            if (!(obj is Client))
+            if (obj is not Client)
                 return false;
 
-            Client other = obj as Client;
+            Client? other = obj as Client;
 
             return Email.Equals(other.Email);
         }
 
-        public override int GetHashCode()
-        {
-            return Email.GetHashCode();
-        }
+        public override int GetHashCode() => Email.GetHashCode();
     }
 }
---
+++ PlayingWithCollections/Program.cs
@@ -0,0 +1,40 @@
+﻿SortedSet<int> a = new()
+{
+    0, 2, 4, 5, 6, 8, 10
+};
+
+SortedSet<int> b = new()
+{
+    5, 6, 7, 8, 9, 10
+};
+
+// Print collection
+PrintCollection(a);
+
+// union
+SortedSet<int> c = new(a);
+c.UnionWith(b);
+
+PrintCollection(c);
+
+// intersection
+SortedSet<int> d = new(a);
+
+d.IntersectWith(b);
+
+PrintCollection(d);
+
+// difference
+SortedSet<int> e = new(a);
+
+e.ExceptWith(b);
+
+PrintCollection(e);
+
+static void PrintCollection<T>(IEnumerable<T> collection)
+{
+    foreach (T obj in collection)
+        Console.Write(obj + " ");
+
+    Console.WriteLine();
+}
