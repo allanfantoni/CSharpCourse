c174adf9d3ed3e63d54b9f6cf4fb6254e6e45c61
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-22 18:15:59 -0300

    Calculator app using delegate

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using Calculator.Services;
+
+namespace Calculator
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Calculator/Services/CalculationService.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator.Services
+{
+    public class CalculationService
+    {
+        public static double Max(double x, double y) => (x > y) ? x : y;
+
+        public static double Sum(double x, double y) => x + y;
+
+        public static double Square(double x) => x * x;
+    }
+}
