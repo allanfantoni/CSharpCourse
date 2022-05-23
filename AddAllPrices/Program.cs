3f9c81584274f8f5b414aed520afdac4d5589579
Allan Fantoni <allan.fantoni@outlook.com>
2022-05-23 18:48:25 -0300

    Add 10% using Action

---
+++ AddAllPrices/Program.cs
@@ -0,0 +1,23 @@
+﻿using AddAllPrices.Entities;
+
+List<Product> list = new()
+{
+    new Product("Tv", 900.00),
+    new Product("Mouse", 50.00),
+    new Product("Tablet", 350.50),
+    new Product("HD Case", 80.90),
+};
+
+//Action<Product> act = UpdatePrice;
+//Action<Product> act = p => { p.Price += p.Price * 0.1; };
+
+//list.ForEach(act);
+//list.ForEach(UpdatePrice);
+list.ForEach(p => { p.Price += p.Price * 0.1; });
+
+foreach (Product product in list)
+{
+    Console.WriteLine(product);
+}
+
+//static void UpdatePrice(Product p) => p.Price += p.Price * 0.1;
--- RemoveMinimumPrice/Program.cs
+++ RemoveMinimumPrice/Program.cs
@@ -1,25 +1,17 @@
 ﻿using RemoveMinimumPrice.Entities;
 
-namespace RemoveMinimumPrice
+List<Product> list = new()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Product> list = new();
+    new Product("Tv", 900.00),
+    new Product("Mouse", 50.00),
+    new Product("Tablet", 350.50),
+    new Product("HD Case", 80.90),
+};
 
-            list.Add(new Product("Tv", 900.00));
-            list.Add(new Product("Mouse", 50.00));
-            list.Add(new Product("Tablet", 350.50));
-            list.Add(new Product("HD Case", 80.90));
+//list.RemoveAll(p => p.Price >= 100.0);
+list.RemoveAll(ProductTest);
 
-            //list.RemoveAll(p => p.Price >= 100.0);
-            list.RemoveAll(ProductTest);
+foreach (Product product in list)
+    Console.WriteLine(product);
 
-            foreach (Product product in list)
-                Console.WriteLine(product);
-        }
-
-        public static bool ProductTest(Product p) => p.Price >= 100.0;
-    }
-}
+static bool ProductTest(Product p) => p.Price >= 100.0;
